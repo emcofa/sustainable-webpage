b38875e73d23881ea09f4c4c07b22219245692c0
fridavbg <fperssontech@gmail.com>
2023-04-19 09:37:34 +0200

    Content & information components added more CSS

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,27 +1,42 @@
-import React from 'react'
-import Lake from '../../assets/lake-unsplash.jpg'
-import './content.css'
+import React from "react";
+import Sun from "../../assets/sun.jpg";
+import "./content.css";
 
 function Content() {
     return (
-        <div className='content__container'>
-            <div className='flex__container'>
+        <div className="content__container">
+            <div className="flex__container">
                 <div>
                     <h3>Lorem ipsum</h3>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id ipsum dui. Nunc maximus est sit amet dui efficitur, id dapibus augue suscipit. Pellentesque volutpat arcu eu justo consectetur hendrerit. Fusce et ex sem. Sed nec ligula quis augue pellentesque vulputate. Duis ac nisi at sem aliquam bibendum eu non lacus. Ut eget consectetur nunc. Donec eu nulla vel mauris vestibulum bibendum. Nam sit amet leo non arcu facilisis malesuada non a quam. Duis id dapibus enim, a hendrerit ex. Sed ornare enim vel dolor iaculis, eu vestibulum est luctus. Curabitur eget tellus imperdiet, eleifend sapien eget, laoreet felis. Nam auctor nunc sit amet quam iaculis, ac mollis odio pulvinar. In hac habitasse platea dictumst.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Sed id ipsum dui. Nunc maximus est sit amet dui
+                        efficitur, id dapibus augue suscipit. Pellentesque
+                        volutpat arcu eu justo consectetur hendrerit. Fusce et
+                        ex sem. Sed nec ligula quis augue pellentesque
+                        vulputate. Duis ac nisi at sem aliquam bibendum eu non
+                        lacus. Ut eget consectetur nunc. Donec eu nulla vel
+                        mauris vestibulum bibendum. Nam sit amet leo non arcu
+                        facilisis malesuada non a quam. Duis id dapibus enim, a
+                        hendrerit ex. Sed ornare enim vel dolor iaculis, eu
+                        vestibulum est luctus. Curabitur eget tellus imperdiet,
+                        eleifend sapien eget, laoreet felis. Nam auctor nunc sit
+                        amet quam iaculis, ac mollis odio pulvinar. In hac
+                        habitasse platea dictumst.
                     </p>
                     <br />
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id ipsum dui. Nunc maximus est sit amet dui efficitur, id dapibus augue suscipit. Pellentesque volutpat arcu eu justo consectetur hendrerit. Fusce et ex sem. Sed nec ligula quis augue pellentesque vulputate. Duis ac nisi at sem aliquam bibendum eu non lacus. Ut eget consectetur nunc. Donec eu nulla vel mauris vestibulum bibendum. Nam sit amet leo non arcu facilisis malesuada non a quam. Duis id dapibus enim, a hendrerit ex. Sed ornare enim vel dolor iaculis, eu vestibulum est luctus.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Sed id ipsum dui. Nunc maximus est sit amet dui
+                        efficitur, id dapibus augue suscipit.
                     </p>
                 </div>
                 <div>
-                    <img src={Lake} alt="lake-unsplash" />
+                    <img src={Sun} alt="sunset" />
                 </div>
             </div>
-        </div >
-    )
+        </div>
+    );
 }
 
-export default Content
+export default Content;
