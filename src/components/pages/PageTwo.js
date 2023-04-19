03576294b4d777af1b9f1bf6c2fe67db6d7517ed
fridavbg <fperssontech@gmail.com>
2023-04-19 09:01:11 +0200

    Added component to pageTwo

--- src/components/pages/PageTwo.js
+++ src/components/pages/PageTwo.js
@@ -1,15 +1,24 @@
-// import Content from "../content/Content";
-// import Video from "../video/Video";
-// import Footer from "../footer/Footer";
-// import Hero from "../hero/Hero";
-// import Information from "../information/Information";
-// import Columns from "../columns/Columns";
-// import Image from "../image/Image";
+import Content from "../content/Content";
+import Video from "../video/Video";
+import Footer from "../footer/Footer";
+import Hero from "../hero/Hero";
+import Information from "../information/Information";
+import Columns from "../columns/Columns";
+import Image from "../image/Image";
 
 const PageTwo = () => {
     return (
         <>
-            <p>PageTwo</p>
+            <div className="nav__above">
+                <p>This is the template for webpage 2</p>
+            </div>
+            <Video />
+            <Content />
+            <Hero />
+            <Information />
+            <Columns />
+            <Image />
+            <Footer />
         </>
     );
 };
